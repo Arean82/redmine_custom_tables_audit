69a677f476d423223201d9738b786c39acfc0164
arean82 <arean82@gmail.com>
2025-11-03 13:32:25 +0530

    updated settings

--- app/controllers/audit_settings_controller.rb
+++ app/controllers/audit_settings_controller.rb
@@ -1,14 +1,16 @@
 class AuditSettingsController < ApplicationController
+  layout 'admin'
   before_action :require_admin
 
   def index
     @settings = Setting.plugin_redmine_custom_tables_audit || {}
+    render :layout => !request.xhr?
   end
 
   def update
     settings = params[:settings] || {}
     Setting.plugin_redmine_custom_tables_audit = settings
     flash[:notice] = l(:notice_successful_update)
-    redirect_to action: 'index'
+    redirect_to settings_plugin_path('redmine_custom_tables_audit')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
+#RedmineApp::Application.routes.draw do
+#  get 'audit_settings', to: 'audit_settings#index'
+#  post 'audit_settings', to: 'audit_settings#update'
+#end
+
 RedmineApp::Application.routes.draw do
-  get 'audit_settings', to: 'audit_settings#index'
-  post 'audit_settings', to: 'audit_settings#update'
+  match 'settings/plugin/redmine_custom_tables_audit', to: 'audit_settings#index', via: :get
+  match 'settings/plugin/redmine_custom_tables_audit', to: 'audit_settings#update', via: :post
 end
+
