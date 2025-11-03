58a91f703df054aeb85be27e1a43a933d59fe117
arean82 <arean82@gmail.com>
2025-11-03 12:42:41 +0530

    updated build issue

--- init.rb
+++ init.rb
@@ -11,8 +11,21 @@ Redmine::Plugin.register :redmine_custom_tables_audit do
     'enable_created_at' => '1'
   },
   partial: 'audit_settings/index'
+  
+  # Ensure this plugin loads AFTER custom_tables plugin
+  requires_redmine_plugin :custom_tables, version_or_higher: '0.0.1'
 end
 
-# Load the patch
-require_dependency 'custom_tables/record'
-CustomTables::Record.include(RedmineCustomTablesAudit::RecordPatch) if defined?(CustomTables::Record)
+# Safe loading - only patch if custom_tables is available
+Rails.configuration.to_prepare do
+  begin
+    if defined?(CustomTables::Record)
+      require_dependency 'redmine_custom_tables_audit/record_patch'
+      CustomTables::Record.include(RedmineCustomTablesAudit::RecordPatch)
+    else
+      Rails.logger.info "Custom Tables plugin not found - Custom Tables Audit plugin disabled"
+    end
+  rescue => e
+    Rails.logger.error "Error loading Custom Tables Audit: #{e.message}"
+  end
+end
